5dc1dad1efd9f1b0619ccbefc7c300225a9d3c76
BuildTools <unconfigured@null.spigotmc.org>
2019-12-02 22:33:25 +0100

    Ajout de la class Position

---
+++ micro-services/src/Delivery/Position.php
@@ -0,0 +1,29 @@
+<?php
+
+class Position
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $parcelNumber;
+
+    /**
+     * @var float
+     */
+    protected $latitude;
+
+    /**
+     * @var float
+     */
+    protected $longitude;
+
+    /**
+     * @var DateTime
+     */
+    protected $date;
+}
